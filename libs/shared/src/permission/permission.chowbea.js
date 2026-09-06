8e14526d0f909ebb98d53f7ad1c9a19c4f08a95a
Allenade <your.email@example.com>
2026-09-06 06:58:01 +0100

    Stop publishing editor and Cursor tooling folders.
    Keep local .cursor/.vscode private; remove accidental JS emit from shared.

--- libs/shared/src/permission/permission.chowbea.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Resource = exports.Action = void 0;
-var permission_enum_1 = require("./permission.enum");
-Object.defineProperty(exports, "Action", { enumerable: true, get: function () { return permission_enum_1.Action; } });
-Object.defineProperty(exports, "Resource", { enumerable: true, get: function () { return permission_enum_1.Resource; } });
-//# sourceMappingURL=permission.chowbea.js.map
